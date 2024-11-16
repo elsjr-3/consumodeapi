8d74f956b25d14d0f53affc245681e80ba271b80
elsajr <elsa10.03.2003@gmail.com>
2024-11-15 23:57:11 -0600

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: UserListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
